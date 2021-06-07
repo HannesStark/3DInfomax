31f574d378989740539e64ade79c76b2cfcf0b58
Hannes Stärk <hannes.staerk@gmail.com>
2021-06-07 08:13:59 +0200

    config

--- train.py
+++ train.py
@@ -124,8 +124,8 @@ def train_qm9(args, device, metrics_dict):
     val_idx = all_idx[len(model_idx) + len(test_idx):]
     train_idx = model_idx[:args.num_train]
     # for debugging purposes:
-    test_idx = all_idx[len(model_idx): len(model_idx) + 200]
-    val_idx = all_idx[len(model_idx) + len(test_idx): len(model_idx) + len(test_idx) + 3000]
+    # test_idx = all_idx[len(model_idx): len(model_idx) + 200]
+    # val_idx = all_idx[len(model_idx) + len(test_idx): len(model_idx) + len(test_idx) + 3000]
 
     model = globals()[args.model_type](node_dim=all_data[0][0].ndata['f'].shape[1],
                                        edge_dim=all_data[0][0].edata['w'].shape[1] if args.use_e_features else 0,
@@ -228,7 +228,7 @@ def train_qm9(args, device, metrics_dict):
 
 def parse_arguments():
     p = argparse.ArgumentParser()
-    p.add_argument('--config', type=argparse.FileType(mode='r'), default='configs/contrastive_distance_aggregator.yml')
+    p.add_argument('--config', type=argparse.FileType(mode='r'), default='configs/2.yml')
     p.add_argument('--experiment_name', type=str, help='name that will be added to the runs folder output')
     p.add_argument('--logdir', type=str, default='runs', help='tensorboard logdirectory')
     p.add_argument('--num_epochs', type=int, default=2500, help='number of times to iterate through all samples')
