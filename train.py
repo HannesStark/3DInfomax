9ace3c1c176392f980d8463919dab81078718ad9
Hannes Stärk <hannes.staerk@gmail.com>
2021-06-08 06:46:47 +0200

    correction of debug code

--- train.py
+++ train.py
@@ -151,6 +151,7 @@ def train_qm9(args, device, metrics_dict):
         model.load_state_dict(model_state_dict)
     print('model trainable params: ', sum(p.numel() for p in model.parameters() if p.requires_grad))
 
+    print(f'Training on {len(train_idx)} samples from the model sequences')
     collate_function = globals()[args.collate_function]
 
     if args.train_sampler != None:
