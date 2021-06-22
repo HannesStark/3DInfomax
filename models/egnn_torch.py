c259c07aa017be41f10d3af0deddb0e8854cf484
Hannes Stärk <hannes.staerk@gmail.com>
2021-06-22 08:22:56 +0200

    config

---
+++ models/egnn_torch.py
@@ -0,0 +1,341 @@
+from typing import List
+
+import dgl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import dgl.function as fn
+
+from commons.utils import fourier_encode_dist
+from models.base_layers import MLP
+
+
+class EGNN(nn.Module):
+    def __init__(self, node_dim, edge_dim, hidden_dim, target_dim, readout_aggregators: List[str], batch_norm=False,
+                 readout_batchnorm=True
+                 , batch_norm_momentum=0.1, reduce_func='sum',
+                 dropout=0.0, propagation_depth: int = 4, readout_layers: int = 2, readout_hidden_dim=None,
+                 fourier_encodings=0,
+                 mid_activation: str = 'SiLU', **kwargs):
+        super(EGNN, self).__init__()
+        self.fourier_encodings = fourier_encodings
+        self.input = MLP(
+            in_dim=node_dim,
+            hidden_size=hidden_dim,
+            out_dim=hidden_dim,
+            mid_batch_norm=batch_norm,
+            last_batch_norm=batch_norm,
+            batch_norm_momentum = batch_norm_momentum,
+            layers=1,
+            mid_activation=mid_activation,
+            dropout=dropout,
+            last_activation='None',
+        )
+
+        self.mp_layers = nn.ModuleList()
+        edge_in_dim = 1 if fourier_encodings == 0 else 2 * fourier_encodings + 1
+        for _ in range(propagation_depth):
+            self.mp_layers.append(
+                EGCLayer(node_dim, edge_dim=edge_in_dim, hidden_dim=hidden_dim, batch_norm=batch_norm, batch_norm_momentum = batch_norm_momentum, dropout=dropout,
+                         mid_activation=mid_activation, reduce_func=reduce_func))
+
+        self.node_wise_output_network = MLP(
+            in_dim=hidden_dim,
+            hidden_size=hidden_dim,
+            out_dim=hidden_dim,
+            mid_batch_norm=batch_norm,
+            last_batch_norm=batch_norm,
+            batch_norm_momentum=batch_norm_momentum,
+            layers=2,
+            mid_activation=mid_activation,
+            dropout=dropout,
+            last_activation='None',
+        )
+        if readout_hidden_dim == None:
+            readout_hidden_dim = hidden_dim
+        self.readout_aggregators = readout_aggregators
+        self.output = MLP(in_dim=hidden_dim * len(self.readout_aggregators),
+                          hidden_size=readout_hidden_dim,
+                          mid_batch_norm=readout_batchnorm,
+                          batch_norm_momentum = batch_norm_momentum,
+                          out_dim=target_dim,
+                          layers=readout_layers)
+
+    def forward(self, graph: dgl.DGLGraph):
+        graph.apply_nodes(self.input_node_func)
+        if self.fourier_encodings > 0:
+            graph.edata['d'] = fourier_encode_dist(graph.edata['d'], num_encodings=self.fourier_encodings).squeeze()
+
+        for mp_layer in self.mp_layers:
+            mp_layer(graph)
+
+        graph.apply_nodes(self.output_node_func)
+
+        readouts_to_cat = [dgl.readout_nodes(graph, 'feat', op=aggr) for aggr in self.readout_aggregators]
+        readout = torch.cat(readouts_to_cat, dim=-1)
+        return self.output(readout)
+
+    def output_node_func(self, nodes):
+        return {'feat': self.node_wise_output_network(nodes.data['feat'])}
+
+    def input_node_func(self, nodes):
+        return {'feat': F.silu(self.input(nodes.data['feat']))}
+
+    def input_edge_func(self, edges):
+        return {'d': F.silu(self.edge_input(edges.data['d']))}
+
+
+
+class E_GCL(nn.Module):
+    """Graph Neural Net with global state and fixed number of nodes per graph.
+    Args:
+          hidden_dim: Number of hidden units.
+          num_nodes: Maximum number of nodes (for self-attentive pooling).
+          global_agg: Global aggregation function ('attn' or 'sum').
+          temp: Softmax temperature.
+    """
+
+    def __init__(self, input_nf, output_nf, hidden_nf, edges_in_d=0, nodes_att_dim=0, act_fn=nn.ReLU(), recurrent=True, coords_weight=1.0, attention=False, clamp=False, norm_diff=False, tanh=False):
+        super(E_GCL, self).__init__()
+        input_edge = input_nf * 2
+        self.coords_weight = coords_weight
+        self.recurrent = recurrent
+        self.attention = attention
+        self.norm_diff = norm_diff
+        self.tanh = tanh
+        edge_coords_nf = 1
+
+
+        self.edge_mlp = nn.Sequential(
+            nn.Linear(input_edge + edge_coords_nf + edges_in_d, hidden_nf),
+            act_fn,
+            nn.Linear(hidden_nf, hidden_nf),
+            act_fn)
+
+        self.node_mlp = nn.Sequential(
+            nn.Linear(hidden_nf + input_nf + nodes_att_dim, hidden_nf),
+            act_fn,
+            nn.Linear(hidden_nf, output_nf))
+
+        layer = nn.Linear(hidden_nf, 1, bias=False)
+        torch.nn.init.xavier_uniform_(layer.weight, gain=0.001)
+
+        self.clamp = clamp
+        coord_mlp = []
+        coord_mlp.append(nn.Linear(hidden_nf, hidden_nf))
+        coord_mlp.append(act_fn)
+        coord_mlp.append(layer)
+        if self.tanh:
+            coord_mlp.append(nn.Tanh())
+            self.coords_range = nn.Parameter(torch.ones(1))*3
+        self.coord_mlp = nn.Sequential(*coord_mlp)
+
+
+        if self.attention:
+            self.att_mlp = nn.Sequential(
+                nn.Linear(hidden_nf, 1),
+                nn.Sigmoid())
+
+        #if recurrent:
+        #    self.gru = nn.GRUCell(hidden_nf, hidden_nf)
+
+
+    def edge_model(self, source, target, radial, edge_attr):
+        if edge_attr is None:  # Unused.
+            out = torch.cat([source, target, radial], dim=1)
+        else:
+            out = torch.cat([source, target, radial, edge_attr], dim=1)
+        out = self.edge_mlp(out)
+        if self.attention:
+            att_val = self.att_mlp(out)
+            out = out * att_val
+        return out
+
+    def node_model(self, x, edge_index, edge_attr, node_attr):
+        row, col = edge_index
+        agg = unsorted_segment_sum(edge_attr, row, num_segments=x.size(0))
+        if node_attr is not None:
+            agg = torch.cat([x, agg, node_attr], dim=1)
+        else:
+            agg = torch.cat([x, agg], dim=1)
+        out = self.node_mlp(agg)
+        if self.recurrent:
+            out = x + out
+        return out, agg
+
+    def coord_model(self, coord, edge_index, coord_diff, edge_feat):
+        row, col = edge_index
+        trans = coord_diff * self.coord_mlp(edge_feat)
+        trans = torch.clamp(trans, min=-100, max=100) #This is never activated but just in case it case it explosed it may save the train
+        agg = unsorted_segment_mean(trans, row, num_segments=coord.size(0))
+        coord += agg*self.coords_weight
+        return coord
+
+
+    def coord2radial(self, edge_index, coord):
+        row, col = edge_index
+        coord_diff = coord[row] - coord[col]
+        radial = torch.sum((coord_diff)**2, 1).unsqueeze(1)
+
+        if self.norm_diff:
+            norm = torch.sqrt(radial) + 1
+            coord_diff = coord_diff/(norm)
+
+        return radial, coord_diff
+
+    def forward(self, h, edge_index, coord, edge_attr=None, node_attr=None):
+        row, col = edge_index
+        radial, coord_diff = self.coord2radial(edge_index, coord)
+
+        edge_feat = self.edge_model(h[row], h[col], radial, edge_attr)
+        coord = self.coord_model(coord, edge_index, coord_diff, edge_feat)
+        h, agg = self.node_model(h, edge_index, edge_feat, node_attr)
+        # coord = self.node_coord_model(h, coord)
+        # x = self.node_model(x, edge_index, x[col], u, batch)  # GCN
+        return h, coord, edge_attr
+
+
+class E_GCL_mask(E_GCL):
+    """Graph Neural Net with global state and fixed number of nodes per graph.
+    Args:
+          hidden_dim: Number of hidden units.
+          num_nodes: Maximum number of nodes (for self-attentive pooling).
+          global_agg: Global aggregation function ('attn' or 'sum').
+          temp: Softmax temperature.
+    """
+
+    def __init__(self, input_nf, output_nf, hidden_nf, edges_in_d=0, nodes_attr_dim=0, act_fn=nn.ReLU(), recurrent=True, coords_weight=1.0, attention=False):
+        E_GCL.__init__(self, input_nf, output_nf, hidden_nf, edges_in_d=edges_in_d, nodes_att_dim=nodes_attr_dim, act_fn=act_fn, recurrent=recurrent, coords_weight=coords_weight, attention=attention)
+
+        del self.coord_mlp
+        self.act_fn = act_fn
+
+    def coord_model(self, coord, edge_index, coord_diff, edge_feat, edge_mask):
+        row, col = edge_index
+        trans = coord_diff * self.coord_mlp(edge_feat) * edge_mask
+        agg = unsorted_segment_sum(trans, row, num_segments=coord.size(0))
+        coord += agg*self.coords_weight
+        return coord
+
+    def forward(self, h, edge_index, coord, node_mask, edge_mask, edge_attr=None, node_attr=None, n_nodes=None):
+        row, col = edge_index
+        radial, coord_diff = self.coord2radial(edge_index, coord)
+
+        edge_feat = self.edge_model(h[row], h[col], radial, edge_attr)
+
+        edge_feat = edge_feat * edge_mask
+
+        # TO DO: edge_feat = edge_feat * edge_mask
+
+        #coord = self.coord_model(coord, edge_index, coord_diff, edge_feat, edge_mask)
+        h, agg = self.node_model(h, edge_index, edge_feat, node_attr)
+
+        return h, coord, edge_attr
+
+
+
+class EGNNTorch(nn.Module):
+    def __init__(self, in_node_nf, in_edge_nf, hidden_nf, device='cpu', act_fn=nn.SiLU(), n_layers=4, coords_weight=1.0, attention=False, node_attr=1):
+        super(EGNNTorch, self).__init__()
+        self.hidden_nf = hidden_nf
+        self.device = device
+        self.n_layers = n_layers
+
+        ### Encoder
+        self.embedding = nn.Linear(in_node_nf, hidden_nf)
+        self.node_attr = node_attr
+        if node_attr:
+            n_node_attr = in_node_nf
+        else:
+            n_node_attr = 0
+        for i in range(0, n_layers):
+            self.add_module("gcl_%d" % i, E_GCL_mask(self.hidden_nf, self.hidden_nf, self.hidden_nf, edges_in_d=in_edge_nf, nodes_attr_dim=n_node_attr, act_fn=act_fn, recurrent=True, coords_weight=coords_weight, attention=attention))
+
+        self.node_dec = nn.Sequential(nn.Linear(self.hidden_nf, self.hidden_nf),
+                                      act_fn,
+                                      nn.Linear(self.hidden_nf, self.hidden_nf))
+
+        self.graph_dec = nn.Sequential(nn.Linear(self.hidden_nf, self.hidden_nf),
+                                       act_fn,
+                                       nn.Linear(self.hidden_nf, 1))
+        self.to(self.device)
+
+    def forward(self, h0, x, edges, edge_attr, node_mask, edge_mask, n_nodes):
+        h = self.embedding(h0)
+        for i in range(0, self.n_layers):
+            if self.node_attr:
+                h, _, _ = self._modules["gcl_%d" % i](h, edges, x, node_mask, edge_mask, edge_attr=edge_attr, node_attr=h0, n_nodes=n_nodes)
+            else:
+                h, _, _ = self._modules["gcl_%d" % i](h, edges, x, node_mask, edge_mask, edge_attr=edge_attr,
+                                                      node_attr=None, n_nodes=n_nodes)
+
+        h = self.node_dec(h)
+        h = h * node_mask
+        h = h.view(-1, n_nodes, self.hidden_nf)
+        h = torch.sum(h, dim=1)
+        pred = self.graph_dec(h)
+        return pred.squeeze(1)
+
+
+def unsorted_segment_sum(data, segment_ids, num_segments):
+    result_shape = (num_segments, data.size(1))
+    result = data.new_full(result_shape, 0)  # Init empty result tensor.
+    segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
+    result.scatter_add_(0, segment_ids, data)
+    return result
+
+
+def unsorted_segment_mean(data, segment_ids, num_segments):
+    result_shape = (num_segments, data.size(1))
+    segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
+    result = data.new_full(result_shape, 0)  # Init empty result tensor.
+    count = data.new_full(result_shape, 0)
+    result.scatter_add_(0, segment_ids, data)
+    count.scatter_add_(0, segment_ids, torch.ones_like(data))
+    return result / count.clamp(min=1)
+
+
+def get_edges(n_nodes):
+    rows, cols = [], []
+    for i in range(n_nodes):
+        for j in range(n_nodes):
+            if i != j:
+                rows.append(i)
+                cols.append(j)
+
+    edges = [rows, cols]
+    return edges
+
+
+def get_edges_batch(n_nodes, batch_size):
+    edges = get_edges(n_nodes)
+    edge_attr = torch.ones(len(edges[0]) * batch_size, 1)
+    edges = [torch.LongTensor(edges[0]), torch.LongTensor(edges[1])]
+    if batch_size == 1:
+        return edges, edge_attr
+    elif batch_size > 1:
+        rows, cols = [], []
+        for i in range(batch_size):
+            rows.append(edges[0] + n_nodes * i)
+            cols.append(edges[1] + n_nodes * i)
+        edges = [torch.cat(rows), torch.cat(cols)]
+    return edges, edge_attr
+
+
+if __name__ == "__main__":
+    # Dummy parameters
+    batch_size = 8
+    n_nodes = 4
+    n_feat = 1
+    x_dim = 3
+
+    # Dummy variables h, x and fully connected edges
+    h = torch.ones(batch_size *  n_nodes, n_feat)
+    x = torch.ones(batch_size * n_nodes, x_dim)
+    edges, edge_attr = get_edges_batch(n_nodes, batch_size)
+
+    # Initialize EGNN
+    egnn = EGNN(in_node_nf=n_feat, hidden_nf=32, out_node_nf=1, in_edge_nf=1)
+
+    # Run EGNN
+    h, x = egnn(h, x, edges, edge_attr)
