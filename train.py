14dbcef245d51c84f3bc5e791e164877d9574d02
Hannes Stärk <hannes.staerk@gmail.com>
2021-06-26 06:56:32 +0200

    config

--- train.py
+++ train.py
@@ -266,8 +266,8 @@ def train_qm9(args, device, metrics_dict):
     val_idx = all_idx[len(model_idx) + len(test_idx):]
     train_idx = model_idx[:args.num_train]
     # for debugging purposes:
-    test_idx = all_idx[len(model_idx): len(model_idx) + 200]
-    val_idx = all_idx[len(model_idx) + len(test_idx): len(model_idx) + len(test_idx) + 3000]
+    # test_idx = all_idx[len(model_idx): len(model_idx) + 200]
+    # val_idx = all_idx[len(model_idx) + len(test_idx): len(model_idx) + len(test_idx) + 3000]
 
     model, num_pretrain = load_model(args, data=all_data, device=device)
     if args.pretrain_checkpoint:
