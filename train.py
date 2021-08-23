4420f0d2b03180b02bbff81b21806bed5bffb120
Hannes Stärk <hannes.staerk@gmail.com>
2021-08-23 07:11:12 +0200

    config

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ seaborn.set_theme()
 
 def parse_arguments():
     p = argparse.ArgumentParser()
-    p.add_argument('--config', type=argparse.FileType(mode='r'), default='configs/10.yml')
+    p.add_argument('--config', type=argparse.FileType(mode='r'), default='configs/pnatransformer.yml')
     p.add_argument('--experiment_name', type=str, help='name that will be added to the runs folder output')
     p.add_argument('--logdir', type=str, default='runs', help='tensorboard logdirectory')
     p.add_argument('--num_epochs', type=int, default=2500, help='number of times to iterate through all samples')
@@ -538,8 +538,8 @@ def train_qm9(args, device, metrics_dict):
     val_idx = all_idx[len(model_idx) + len(test_idx):]
     train_idx = model_idx[:args.num_train]
     # for debugging purposes:
-    test_idx = all_idx[len(model_idx): len(model_idx) + 20]
-    val_idx = all_idx[len(model_idx) + len(test_idx): len(model_idx) + len(test_idx) + 30]
+    # test_idx = all_idx[len(model_idx): len(model_idx) + 20]
+    # val_idx = all_idx[len(model_idx) + len(test_idx): len(model_idx) + len(test_idx) + 30]
 
     model, num_pretrain, transfer_from_same_dataset = load_model(args, data=all_data, device=device)
     if transfer_from_same_dataset:
