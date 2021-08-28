3e1d4b9ce5f84b870430643e2632253db97741e0
Hannes Stärk <hannes.staerk@gmail.com>
2021-08-28 19:08:55 +0200

    config

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ seaborn.set_theme()
 
 def parse_arguments():
     p = argparse.ArgumentParser()
-    p.add_argument('--config', type=argparse.FileType(mode='r'), default='configs/28.yml')
+    p.add_argument('--config', type=argparse.FileType(mode='r'), default='configs/27.yml')
     p.add_argument('--experiment_name', type=str, help='name that will be added to the runs folder output')
     p.add_argument('--logdir', type=str, default='runs', help='tensorboard logdirectory')
     p.add_argument('--num_epochs', type=int, default=2500, help='number of times to iterate through all samples')
